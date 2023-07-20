16d5031c15f9c2343eb55e660c33fd23078951b6
divyanshuyadav2 <divyanshu.yadav@bolgatty.nl>
2023-07-21 00:28:24 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {add,sub,mul,div} from './Calc';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<> <ul>
+  <li>The sum of two no. is {add(40,4)}</li>
+  <li>The Subtraction is{sub(40,4)}</li>
+  <li>The multiplication of two no.  {mul(40,4)}</li>
+  <li>The divison of two no is{div(40,4)}</li>
+</ul>
+</>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
